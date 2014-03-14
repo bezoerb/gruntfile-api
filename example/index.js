a1e5b8c8428366872374c093e593e21a76922b44
Ben Zörb <ben@sommerlaune.com>
2014-03-14 13:53:13 +0100

    Implemented option merge for insertConfig

--- example/index.js
+++ example/index.js
@@ -12,39 +12,61 @@
 var api = require('../lib/api.js'),
     fs = require('fs'),
     beautify = require('js-beautify').js_beautify,
-    gruntfile = fs.readFileSync('test/fixtures/plain.js', 'utf-8').toString(),
+    gruntfile = fs.readFileSync('test/fixtures/plain_task.js', 'utf-8').toString(),
     multiline = require('multiline');
 
 
 var output = api.init(gruntfile)
-    .addGlobalDeclaration('test1',[{d:4}])
-    .addGlobalDeclaration('test2',42)
-    .addGlobalDeclaration('test3','abc')
-    .addGlobalDeclaration('test4',function(test){ return test * test; })
-    .registerTask('defaultdefault', [42])
-    .registerTask('default', function(target){
-        grunt.task.run(['jshint']);
-    })
-    .registerTask('test', function(target,t2){
-        if (target === 'build') {
-            grunt.task.run(['watch']);
+    .insertConfig('less',{
+        options: {
+            paths: ['/styles'],
+            optimization: 1,
+            something: 3
+        },
+        dist: {
+            options: {
+                test: 4,
+                optimization: 2
+            },
+            files: [
+                {expand: true, cwd:  '/styles', src: ['*.less'], dest: '.tmp/styles', ext: '.css'}
+            ]
         }
-    },'skip')
-    .registerTask('test', [42])
-    .insertConfig('testtask',{  tests: ['test/**/*_test.js'] })
-    .insertRawConfig('testtask','{ abc : (new Date()).getTime() }')
-    .insertRawConfig('watch', multiline(function(){/*
-        {
-            js: {
-                options: {
-                    time: (new Date()).getTime()
-                },
-                files: MYPREVIOUSDECLAREDFILES,
-                tasks: ['jshint']
-            }
-        }
-    */}))
-    .toString();
+
+    }).toString();
 
 console.log(output);
+
+//
+//var output = api.init(gruntfile)
+//    .addGlobalDeclaration('test1',[{d:4}])
+//    .addGlobalDeclaration('test2',42)
+//    .addGlobalDeclaration('test3','abc')
+//    .addGlobalDeclaration('test4',function(test){ return test * test; })
+//    .registerTask('defaultdefault', [42])
+//    .registerTask('default', function(target){
+//        grunt.task.run(['jshint']);
+//    })
+//    .registerTask('test', function(target,t2){
+//        if (target === 'build') {
+//            grunt.task.run(['watch']);
+//        }
+//    },'skip')
+//    .registerTask('test', [42])
+//    .insertConfig('testtask',{  tests: ['test/**/*_test.js'] })
+//    .insertRawConfig('testtask','{ abc : (new Date()).getTime() }')
+//    .insertRawConfig('watch', multiline(function(){/*
+//        {
+//            js: {
+//                options: {
+//                    time: (new Date()).getTime()
+//                },
+//                files: MYPREVIOUSDECLAREDFILES,
+//                tasks: ['jshint']
+//            }
+//        }
+//    */}))
+//    .toString();
+
+//console.log(output);
 
