c47c564ef379b770a73bbe1ce5703d1a1faaf458
Ben Zörb <ben@sommerlaune.com>
2014-03-27 08:04:30 +0100

    Keep comments in added tasksside

--- example/index.js
+++ example/index.js
@@ -12,70 +12,27 @@
 var api = require('../lib/api.js'),
     fs = require('fs'),
     esformatter = require('esformatter'),
-    gruntfile = fs.readFileSync('test/fixtures/plain.js', 'utf-8').toString(),
+    gruntfile = fs.readFileSync('test/fixtures/Gruntfile.js', 'utf-8').toString(),
     multiline = require('multiline');
 
 
 var output = api.init(gruntfile)
-    .addGlobalDeclarationRaw('path','require(\'path\')')
+    .insertRawConfig('watch', multiline(function(){/*
+        {
+            // ADD TEST COMMENT
+            js: {
+                // main options
+                options: {
+                    time: (new Date()).getTime()
+                },
+                // some other stuff
+                files: MYPREVIOUSDECLAREDFILES,
+                tasks: ['jshint']
+            }
+        }
+    */}))
     .toString();
 
 
 
-//console.log(output);
-//console.log('---------');
-
-// for a list of available options check "lib/preset/default.json"
-var options = {
-    // inherit from the default preset
-    preset : 'default',
-    indent : {
-        value : '  '
-    },
-    lineBreak : {
-        before : {
-            BlockStatement : 1,
-            DoWhileStatementOpeningBrace : 1
-        }
-    },
-    whiteSpace : {
-    }
-};
-
-var formattedCode = esformatter.format(output,options);
-
-console.log(formattedCode);
-
-//
-//var output = api.init(gruntfile)
-//    .addGlobalDeclaration('test1',[{d:4}])
-//    .addGlobalDeclaration('test2',42)
-//    .addGlobalDeclaration('test3','abc')
-//    .addGlobalDeclaration('test4',function(test){ return test * test; })
-//    .registerTask('defaultdefault', [42])
-//    .registerTask('default', function(target){
-//        grunt.task.run(['jshint']);
-//    })
-//    .registerTask('test', function(target,t2){
-//        if (target === 'build') {
-//            grunt.task.run(['watch']);
-//        }
-//    },'skip')
-//    .registerTask('test', [42])
-//    .insertConfig('testtask',{  tests: ['test/**/*_test.js'] })
-//    .insertRawConfig('testtask','{ abc : (new Date()).getTime() }')
-//    .insertRawConfig('watch', multiline(function(){/*
-//        {
-//            js: {
-//                options: {
-//                    time: (new Date()).getTime()
-//                },
-//                files: MYPREVIOUSDECLAREDFILES,
-//                tasks: ['jshint']
-//            }
-//        }
-//    */}))
-//    .toString();
-
-//console.log(output);
-
+console.log(output);
