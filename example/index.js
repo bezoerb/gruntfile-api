b91936fa1af0d39c15986b117f717e8b4a3c459e
Ben Zörb <ben@sommerlaune.com>
2015-02-18 05:37:17 +0100

    minor tweaks

--- example/index.js
+++ example/index.js
@@ -10,14 +10,21 @@
 'use strict';
 
 var api = require('../lib/api.js'),
-    fs = require('fs'),
-    gruntfile = fs.readFileSync('test/fixtures/plain_var_scope.js', 'utf-8').toString(),
-    multiline = require('multiline');
+  fs = require('fs'),
+  gruntfile = fs.readFileSync('test/fixtures/plain_task_withoptions.js', 'utf-8').toString(),
+  multiline = require('multiline');
 
 var output = api.init(gruntfile)
-    .addGlobalDeclarationRaw('path','require("path")')
-    .toString();
+  .insertConfig('less', {options: {optimization: 1, paths: ['/styles']}, newTask: {files: ['test/**/*_test.js']}})
+  .insertConfig('nodeunit', {otherFiles: ['test/**/*_test.js']})
+  .registerTask('createCopy', function(folderName) {
+    var cwd = grunt.template.process("some/path/to/app");
+    var src = grunt.template.process("some/path/to/src");
+    var dest = grunt.template.process("some/path/to/dest") + folderName;
 
+    grunt.task.run('...');
+  })
+  .toString();
 
 
 console.log(output);
